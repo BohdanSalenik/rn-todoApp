2dd46ae1395dc7bee718759a1bc81c78c8083eee
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-04-06 11:49:55 +0300

    Updates

---
+++ precache-manifest.dfc766bbf46e46c048eaa31731c46b4e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "b89293b1d247411d34bb",
+    "url": "/rn-todoApp/static/js/app.c3c01a4a.chunk.js"
+  },
+  {
+    "revision": "72ed70bf96ccee5ca91d",
+    "url": "/rn-todoApp/static/js/runtime~app.588a011b.js"
+  },
+  {
+    "revision": "6422479e283828fa3363",
+    "url": "/rn-todoApp/static/js/2.ba982a52.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/rn-todoApp/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rn-todoApp/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "eba6baadcf83911af0ff853972f7c71b",
+    "url": "/rn-todoApp/static/media/no-items.eba6baad.png"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/rn-todoApp/./fonts/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/rn-todoApp/./fonts/Roboto-Bold.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/rn-todoApp/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/rn-todoApp/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/rn-todoApp/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/rn-todoApp/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/rn-todoApp/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/rn-todoApp/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/rn-todoApp/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/rn-todoApp/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/rn-todoApp/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/rn-todoApp/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/rn-todoApp/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/rn-todoApp/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/rn-todoApp/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/rn-todoApp/favicon.ico"
+  },
+  {
+    "revision": "84731dcc23588c76e4f0cc74053f79d0",
+    "url": "/rn-todoApp/index.html"
+  },
+  {
+    "revision": "6b577320afc1a8fc6238582bc87222af",
+    "url": "/rn-todoApp/manifest.json"
+  },
+  {
+    "revision": "1fa2dcc5d56fc94595ce756915588754",
+    "url": "/rn-todoApp/register-service-worker.js"
+  }
+];
